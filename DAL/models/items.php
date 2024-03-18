2dd4c805840c50c79551595e0ff10a276b3bbd17
DamienNoel2 <DamienNoel2@hotmail.com>
2024-03-18 09:37:59 -0400

    Update items.php

--- DAL/models/items.php
+++ DAL/models/items.php
@@ -1,7 +1,23 @@
 <?php
 include_once 'DAL/models/record.php';
 
-public class Items extends Record
+Class Items extends Record
 {
-    
+    public $nom;
+    public $quantité;
+    public $type;
+    public $prix;
+    public $photo;
+
+    public function __construct($recordData = null)
+    {
+        
+        $this->nom = "";
+        $this->quantité = 1;
+        $this->type = "";
+        $this->prix = 200;
+        $this->photo = "";
+        $this->setUniqueKey('Alias');
+        parent::__construct($recordData);
+    }
 }
