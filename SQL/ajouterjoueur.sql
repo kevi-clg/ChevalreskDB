32ced0dd595b0274dabb25645092801334e5f703
DamienNoel2 <DamienNoel2@hotmail.com>
2024-03-22 08:08:13 -0400

    Update ajouterjoueur.sql

--- SQL/ajouterjoueur.sql
+++ SQL/ajouterjoueur.sql
@@ -1,1 +1,2 @@
-DROP PROCEDURE `AjouterJoueur`; CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterJoueur`(IN `AliasAjout` VARCHAR(20), IN `PrénomAjout` VARCHAR(20), IN `NomAjout` VARCHAR(20), IN `PasswordAjout` VARCHAR(20)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER INSERT into joueurs (alias, prénom,nom,password) VALUES(aliasAjout,PrénomAjout,NomAjout, PasswordAjout)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterJoueur`(IN `AliasAjout` VARCHAR(20), IN `PrénomAjout` VARCHAR(20), IN `NomAjout` VARCHAR(20), IN `PasswordAjout` VARCHAR(20)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER INSERT into joueurs (alias, prénom,nom,password) VALUES(aliasAjout,PrénomAjout,NomAjout, PasswordAjout)
+DROP PROCEDURE `AjouterJoueur`; <--Au besoin-->
