589e3f345e7d4b2af37ab5910df46d7539244da9
DamienNoel2 <DamienNoel2@hotmail.com>
2024-03-22 09:24:35 -0400

    Procedure pour login

--- DAL/functions.php
+++ DAL/functions.php
@@ -18,3 +18,31 @@ function Create_Joueur($alias, $prenom, $nom, $password)
 
         $stmt->execute();
 }
+
+function LoginJoueur($alias, $password)
+{
+    try {
+        $conn = new PDO('mysql:host=localhost;dbname=dbchevalersk18',"root", "");
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    } catch (Throwable $th) {
+        throw new PDOException($th->getMessage()) ;
+    }
+    $stmt = $conn->prepare('CALL LoginJoueur(:alias, :password)');
+    $stmt->bindParam(':alias', $alias);
+    $stmt->bindParam(':password', $password);
+
+    $stmt->execute();
+    $login = $stmt->fetch();
+    if($login[0])
+    {
+        $_SESSION['Id'] = $login["idJoueur"];
+        $_SESSION['Alias'] = $login["alias"];
+        $_SESSION['Prenom'] = $login["prenom"];
+        $_SESSION['Nom'] = $login["nom"];
+        $_SESSION['Solde'] = $login["solde"];
+        $_SESSION['Type'] = $login["typee"];
+        $_SESSION['Demande'] = $login["demande"];
+        $_SESSION['Password'] = $login["password"];
+        $_SESSION['ValidUser'] = true;
+    }
+}
