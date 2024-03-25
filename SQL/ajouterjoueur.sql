9abe9bd80a3cf35bd2ff9de5d8b8572fe97dc506
kevi-clg <202269499@edu.clg.qc.ca>
2024-03-25 09:30:33 -0400

    login joueur

--- SQL/ajouterjoueur.sql
+++ SQL/ajouterjoueur.sql
@@ -7,3 +7,8 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `LoginJoueur`(IN `AliasFind` VARCHAR(20), IN `PasswordFind` VARCHAR(20))
 SELECT * from joueurs where alias = AliasFind AND password = PasswordFind$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `LoginJoueur`(IN `AliasFind` VARCHAR(20), IN `PasswordFind` VARCHAR(20))
+SELECT * from joueurs where alias = AliasFind AND password = PasswordFind$$
+DELIMITER ;
