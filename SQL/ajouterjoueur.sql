17bc20679e5b88725bae3a39ae9ba3886b2fe50a
DamienNoel2 <DamienNoel2@hotmail.com>
2024-03-27 13:04:35 -0400

    Update ajouterjoueur.sql

--- SQL/ajouterjoueur.sql
+++ SQL/ajouterjoueur.sql
@@ -19,3 +19,8 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `LoginJoueur`(IN `AliasFind` VARCHAR(20), IN `PasswordFind` VARCHAR(20))
 SELECT * from joueurs where alias = AliasFind AND password = PasswordFind$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `EditJoueur`(IN `AliasEdit` VARCHAR(20), IN `PrenomEdit` VARCHAR(20), IN `NomEdit` VARCHAR(20), IN `MotdepasseEdit` VARCHAR(20), IN `AvatarEdit` VARCHAR(256), IN `IdJoueurEdit` INT)
+UPDATE joueurs SET alias = AliasEdit, prenom = PrenomEdit, nom = NomEdit, password = MotdepasseEdit, avatar = AvatarEdit where idJoueur = IdJoueurEdit$$
+DELIMITER ;
