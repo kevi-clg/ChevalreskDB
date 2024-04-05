dbdf60bd0fe02612570e567a58ae2c3da74fc13a
kevi-clg <202269499@edu.clg.qc.ca>
2024-04-05 11:29:05 -0400

    sql

--- SQL/insertItem.sql
+++ SQL/insertItem.sql
@@ -13,3 +13,33 @@ begin
     insert into Armes values (id, Efficacite, Genre, DescriptionArme);
 end$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterArmure`(IN `Nom` VARCHAR(20), IN `Quantite` INT, IN `Prix` INT, IN `Photo` VARCHAR(256), IN `matiere` VARCHAR(20), IN `taille` CHAR(1))
+begin
+	declare id int;
+    insert into Items (nom,quantite,typee,prix,photo) values (Nom,Quantite,'Armure',Prix,Photo);
+    set id = (select idItem from Items order by idItem desc LIMIT 1);
+    insert into Armures values (id, matiere, taille);
+end$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterPotion`(IN `Nom` VARCHAR(20), IN `Quantite` INT, IN `Prix` INT, IN `Photo` VARCHAR(256), IN `Effet` VARCHAR(50), IN `Duree` INT, IN `Typee` VARCHAR(20))
+begin
+	declare id int;
+    insert into Items (nom,quantite,typee,prix,photo) values (Nom,Quantite,'Potion',Prix,Photo);
+    set id = (select idItem from Items order by idItem desc LIMIT 1);
+    insert into Potions values (id, Effect, Duree, Typee);
+end$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterElement`(IN `Nom` VARCHAR(20), IN `Quantite` INT, IN `Prix` INT, IN `Photo` VARCHAR(256), IN `Typee` VARCHAR(20), IN `rarete` INT, IN `dangerosite` INT)
+begin
+	declare id int;
+    insert into Items (nom,quantite,typee,prix,photo) values (Nom,Quantite,'Élément',Prix,Photo);
+    set id = (select idItem from Items order by idItem desc LIMIT 1);
+    insert into éléments values (id, Typee, rarete, dangerosite);
+end$$
+DELIMITER ;
