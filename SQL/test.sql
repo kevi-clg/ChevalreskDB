531d8b29cc04e13be2250db9d53eab96d17b1a4b
Louka <95499931+Grimmtroupe@users.noreply.github.com>
2024-04-05 09:30:29 -0400

    test

---
+++ SQL/test.sql
@@ -0,0 +1,16 @@
+/*table joueur*/
+INSERT INTO joueurs (alias, prenom, nom, password, avatar) VALUES ('Louka_J4', 'Louka', 'Janelle', 'password', 'data/images/avatars/Louka-Icon-092221.png')
+INSERT INTO joueurs (alias, prenom, nom, password, avatar) VALUES ('Kévi_S5', 'Kévi', 'Simard', 'password', 'data/images/avatars/logo.jpg')
+INSERT INTO joueurs (alias, prenom, nom, password, avatar) VALUES ('Damien_N6', 'Damien', 'Noël', 'password', 'data/images/avatars/Damien-Icon-092159.png')
+INSERT INTO joueurs (alias, prenom, nom, password, avatar) VALUES ('Prof_cool103', 'Luc', 'Ledoux', 'password', 'data/images/avatars/vector-valid-icon-design.jpg')
+INSERT INTO joueurs (alias, prenom, nom, password, avatar) VALUES ('Wilson23', 'Tom', 'Hanks', 'password', 'data/images/avatars/Ballon-Icon- 092550.png')
+
+/*table Armes*/
+
+
+/*table Armures*/
+
+
+/*table Potions*/
+
+/*table Elements*/
