52cc4ff8c4b254ac4737cef8e0540f797e1b6ccf
kevi-clg <202269499@edu.clg.qc.ca>
2024-04-09 09:25:33 -0400

    panier

--- SQL/panier.sql
+++ SQL/panier.sql
@@ -8,3 +8,25 @@ BEGIN
 	where idJoueur = joueur_connecter;
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterPanier`(IN `idJoueurVariable` INT, IN `idItemVariable` INT)
+BEGIN
+    DECLARE existing_quantity INT;
+    
+    -- Vérifier si l'item existe déjà dans le panier du joueur
+    SELECT quantite INTO existing_quantity
+    FROM panier
+    WHERE idJoueur = idJoueurVariable AND idItem = idItemVariable;
+    
+    IF existing_quantity IS NOT NULL THEN
+        -- Si l'item existe déjà, mettre à jour la quantité
+        UPDATE panier
+        SET quantite = 1 + existing_quantity
+        WHERE idJoueur = idJoueurVariable AND idItem = idItemVariable;
+    ELSE
+        -- Si l'item n'existe pas, l'ajouter au panier
+        INSERT INTO panier VALUES (idJoueurVariable, idItemVariable, 1);
+    END IF;
+END$$
+DELIMITER ;
