7bb3b3bfe664c14a6169976f86e5248edf230b94
DamienNoel2 <DamienNoel2@hotmail.com>
2024-05-17 08:14:21 -0400

    proce

--- SQL/EnigmaProcedures.sql
+++ SQL/EnigmaProcedures.sql
@@ -29,6 +29,25 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AjouterSolde`(IN `idJoueurPara` INT, IN `soldebonus` INT)
 UPDATE joueurs set solde = solde + soldebonus where idJoueur = idJoueurPara$$
 DELIMITER ;
+
+DROP TABLE IF EXISTS `enigmastats`;
+CREATE TABLE IF NOT EXISTS `enigmastats` (
+  `Diff` int NOT NULL,
+  `Bonne` int NOT NULL,
+  `Mauvaise` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `enigmastats`
+--
+
+INSERT INTO `enigmastats` (`Diff`, `Bonne`, `Mauvaise`) VALUES
+(1, 5, 2),
+(2, 2, 0),
+(3, 3, 2);
+COMMIT;
+
+
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CountQuestions`(IN `IdJoueurPara` INT)
 Select COUNT(*) as Count from enigmesrepondues left join enigmes on enigmesrepondues.idQuestion = enigmes.idQuestion where IdJoueur = IdJoueurPara && typee = 'P'$$
